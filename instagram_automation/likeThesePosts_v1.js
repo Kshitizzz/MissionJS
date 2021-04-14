227fa251c3064e4f6a1ea1188e3e4fb319afd58d
Kshitiz <kshitizomar@gmail.com>
2021-04-14 23:18:10 +0530

    added comments, cleaned up extra spaces, linebreak

--- instagram_automation/likeThesePosts_v1.js
+++ instagram_automation/likeThesePosts_v1.js
@@ -1,14 +1,13 @@
 // input the username of an instagram profile, goto it, and like the mentioned number of posts there
-// (also print the names that were liked)
 // goto https://instagram.com --> click on the search bar --> type the username(exact)
 // click on the top search result --> go to the profile
-// like the mentioned number of posts --> store description of each liked post in an arr
-// print that arr 
+// like the mentioned number of posts
 
 let puppeteer = require('puppeteer');
 let fs = require('fs');
+// crediantials of my insta account stored in a different file for privacy
 let {username, password} = require("./crediantials.js");
-
+// take input from the user, the user name of the profile
 let userName = process.argv.slice(2)[0];
 let numberOfPostsToLike = process.argv.slice(2)[1];
 
@@ -34,17 +33,24 @@ let numberOfPostsToLike = process.argv.slice(2)[1];
         await newPage.waitForSelector(loginButtonSelector);
         await newPage.click(loginButtonSelector, {delay : 200});
         await newPage.waitForNavigation();
+        // login done
+        // now to close insta save login info popup
         await newPage.waitForSelector("button[type = 'button']") 
         await newPage.click("button[type = 'button']")
         await newPage.waitForNavigation();
+        // to close the turn on notifications popup
         await newPage.waitForSelector(".aOOlW.HoLwm");
         await newPage.click(".aOOlW.HoLwm");
+        // wait for search bar to load fully
         await newPage.waitForSelector(searchBarSelector);
         await newPage.type(searchBarSelector, userName, {delay : 200});
+        // wait for the searched profile result to show up
         await newPage.waitForSelector("a[class = '-qQT3']", {visible : true});
+        // click on the top result and go to that profile page
         await newPage.click("a[class = '-qQT3']");
         // wait for post selector to show up/load
         await newPage.waitForSelector('div[class="u7YqG"]');
+        // click on the first post
         await newPage.click('div[class="u7YqG"]');
         
         while(numberOfPostsToLike > 0){
@@ -59,6 +65,6 @@ let numberOfPostsToLike = process.argv.slice(2)[1];
             numberOfPostsToLike -= 1;
             }
     }catch(err) {console.log(err)};
-})()
+})();
 
 
